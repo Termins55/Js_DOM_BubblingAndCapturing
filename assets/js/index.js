0255b867f1ea0bfc1edb11c1cca0cc4bc5365bf3
Termins55 <kevgame123@gmail.com>
2025-08-19 17:08:02 +0300

    Add classList

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,12 @@
-// Керування розповсюдженням подій
+const div = document.querySelector(".div");
 
-const body = document.body;
-const section = document.querySelector("section");
-const button = document.querySelector("button");
-
-// function clickHandler(e) {
-//   console.log("this :>> ", this); // = currentTarget
-// //   console.log("e.target :>> ", e.target); // ціль подіїї, найбільш занурений елемент
-// //   console.log("e.currentTarget :>> ", e.currentTarget);
-//   console.log("---------------------------------------");
-
-//   e.target.style.backgroundColor = "red";
-// }
-// // Bubbling and capturing / порядок виклику обробників
-
-// // на занурення (capturing)
-// body.addEventListener("click", clickHandler, { capture: true });
-// section.addEventListener("click", clickHandler, { capture: true });
-// button.addEventListener("click", clickHandler, { capture: true });
-
-// // на сплиття (bubbling)
-// button.addEventListener("click", clickHandler);
-// section.addEventListener("click", clickHandler);
-// body.addEventListener("click", clickHandler);
-
-// Припинення розповсюдження події
-
-let clickCounter = 0;
-
-body.addEventListener(
-  "click",
-  () => {
-    clickCounter++;
-    console.log("clickCounter :>> ", clickCounter);
-  },
-//   { capture: true }
-);
-
-button.addEventListener("click", (e) => {
-  e.stopPropagation(); // припинення розповсюдження події
+div.addEventListener("click", function () {
+  //   this.classList.add("div2");
+  //   this.classList.remove("div");
+  this.classList.toggle("div2");
 });
 
-// body capture
-// section capture
-// btn capture
+div.style.color = "red";
 
-// btn X
-// section
-// body
+// класи - рядок з іменами класів, розділеними пробілами
+// div.className = "div div2";
